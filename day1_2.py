56de4e33a65373baa908873380aa2aea76489bf3
Maciej Byczko <byczqq136@gmail.com>
2021-12-01 20:16:08 +0100

    day 1
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

---
+++ day1_2.py
@@ -0,0 +1,30 @@
+import sys
+
+window_size = 3
+
+
+def main(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        # convert values in array to integers
+        int_lines = [int(i) for i in lines]
+        print(count_increase_window(int_lines))
+
+
+# count how many times next value is higher than previous in given window (window_size)
+def count_increase_window(data):
+    counter = 0
+    for index in range(len(data)-3):
+        if sum(data[index+1:index+window_size+1]) - sum(data[index:index+window_size]) > 0:
+            counter += 1
+    return counter
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:  # if user didn't give enough arguments, display message
+        print("\033[1;32m"
+              "Usage: python.exe " + sys.argv[0] + " <filename>"
+              "\033[1;m")
+        sys.exit(0)
+    filename = sys.argv[1]
+    main(filename)
