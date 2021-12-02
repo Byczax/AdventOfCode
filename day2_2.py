118f421785b7e695e85236913f0e34297500fa30
Maciej Byczko <byczqq136@gmail.com>
2021-12-02 15:22:40 +0100

    day 2
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

---
+++ day2_2.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def main(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        position = count_position(lines)
+        print(position["depth"] * position["horizontal"])
+
+
+# count how many times next value is higher than previous
+def count_position(data):
+    params = {
+        "depth": 0,
+        "horizontal": 0,
+        "aim": 0
+    }
+    for line in data:
+        position, value = line.split(" ")
+        value = int(value)
+        if position == "forward":
+            params["horizontal"] += value
+            if params["aim"] != 0:
+                params["depth"] += value * params["aim"]
+        elif position == "down":
+            params["aim"] += value
+        else:
+            params["aim"] -= value
+    return params
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:  # if user didn't give enough arguments, display message
+        print("\033[1;32m"
+              "Usage: python.exe " + sys.argv[0] + " <filename>"
+              "\033[1;m")
+        sys.exit(0)
+    filename = sys.argv[1]
+    main(filename)
