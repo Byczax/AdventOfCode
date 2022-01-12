136450d7d78e350cbb1c4c002b8453a8e0623145
Maciej Byczko <byczqq136@gmail.com>
2022-01-12 18:27:58 +0100

    Connect python solution into one file
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- day01/day1.py
+++ day01/day1.py
@@ -2,23 +2,31 @@ import sys
 
 window_size = 3
 
-
 def main(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
         # convert values in array to integers
         int_lines = [int(i) for i in lines]
+        print(count_increase(int_lines))
         print(count_increase_window(int_lines))
 
 
-# count how many times next value is higher than previous in given window (window_size)
+# count how many times next value is higher than previous
+def count_increase(data):
+    counter = 0
+    previous = data[0]  # first element
+    for line in data[1:]:  # get every line, skip first element
+        if line - previous > 0:
+            counter += 1
+        previous = line
+    return counter
+
 def count_increase_window(data):
     counter = 0
     for index in range(len(data)-3):
         if sum(data[index+1:index+window_size+1]) - sum(data[index:index+window_size]) > 0:
             counter += 1
     return counter
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:  # if user didn't give enough arguments, display message
